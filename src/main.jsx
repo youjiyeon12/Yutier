e097b2e397b0bb6d429e683e05340c3a0b402bf9
youjiyeon12 <youjiyeon12@gmail.com>
2025-11-12 11:04:41 +0900

    Add GitHub Pages deployment settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; 
+import App from './App.jsx';
 import './global.css';
 
-// React앱의 시작점
+// React 앱의 시작점
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
