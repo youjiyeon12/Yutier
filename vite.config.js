e1b42b02baa7d8ed619f977eb86766b8af2020f1
youjiyeon12 <127164370+youjiyeon12@users.noreply.github.com>
2025-11-23 20:21:34 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //GitHub pages용 경로 설정
+  //GitHub Actions용 경로 설정
   base: '/Yutier/',
   //개발 서버 설정(로컬에서만 사용됨)
   server: {
